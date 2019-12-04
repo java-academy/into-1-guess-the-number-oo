ee4d000345c2c220af87026e0ed13de1f123c935
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2019-12-04 16:10:02 +0100

    Hints in imperative version
    C programmers never die. They are just cast into void.

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -5,10 +5,16 @@ class GuessTheNumber {
 
     public static void main(String[] args) {
         System.out.println("Welcome in Guess the Number guessing game!");
-        final int secretNumber = new Random().nextInt(10)+ 1;
+        final int secretNumber = new Random().nextInt(100)+ 1;
         int userNumber = -1;
         do {
             userNumber = new Scanner(System.in).nextInt();
+            if (userNumber > secretNumber) {
+                System.out.println("Too high!")
+            }
+            if (userNumber < secretNumber) {
+                System.out.println("Too low!")
+            }
         } while (userNumber != secretNumber);
 
     }
